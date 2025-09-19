14349d9dc66d2a2c7724eae0eebb7ed4a6dcddb5
puran dubey <dubeypuran2002@gmail.com>
2025-09-19 06:23:25 -0700

    something

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home/Home';
 const App = () => {
   return (
      <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} /> 
     </Routes>
   )
 }
